41347afce7082a6d40384163ca86734a0d195cc0
Giulia Verra <Giulia Verra@LAPTOP-OPJD32A5>
2019-03-11 13:53:36 +0100

    Progetto inziale. Classe voto vuota.

---
+++ src/it/polito/tdp/libretto/Voto.java
@@ -0,0 +1,16 @@
+package it.polito.tdp.libretto;
+
+import java.time.LocalDate;		//importare libreria date
+
+public class Voto {
+
+	
+	private int voto;
+	private String nomeCorso;
+	private LocalDate data;		//oggetto date, calendar era l'unitco modo di gestire le date fino a java 6
+								//da java 7 c'è una libreria nuova che permette di lavorare con le date
+	//per salvare  
+	
+	
+	
+}
